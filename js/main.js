06bebb73cf638f0b48048ea926371332685a7369
Evgeniy Kirichenko <evgenkirichenko@gmail.com>
2018-05-18 08:52:37 +0300

    v.1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($) {
+	"use strict";
+	$(function(){
+		var $fullScreenSlider = $('.ba-slider');
+		$fullScreenSlider.slick({
+			dots: true,
+			infinite: false
+		});
+	})
+
+})(jQuery);
+
